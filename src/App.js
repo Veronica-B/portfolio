e647a1218648147472e39ab0c4ad54b07f255b99
Veronica Barcia <veronicabarcia4@gmail.com>
2020-07-13 16:00:53 -0400

    resume portion complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Vero from "./img/Vero.jpg";
+import Resume from "./img/resume-pic.png"
 import NavBar from "./components/navBar.js"
 import "./css/styles.css"
 
@@ -20,6 +21,24 @@ function App() {
   
       <section className="background--pink">
       <h2>Resume</h2>
+      <div className="container--resume">
+   <a className="a--resume"href="https://docs.google.com/document/d/1vZwJYfSS7kVToykG_XUi8ld_kagfFvN1T0HioIQB5qE/edit"><img className="pic--resume" src={Resume} alt="resume"/></a> 
+    <ul className="ul--resume">Hard Skills
+      <li>HTML 5</li>
+      <li>CSS (SASS)</li>
+      <li>Javascript</li>
+      <li>Mysql</li>
+    </ul>
+
+    <ul className="ul--resume">Soft Skills
+      <li>Teamwork</li>
+      <li>Public Speaking</li>
+      <li>Fast Learner</li>
+      <li>Time Management</li>
+    </ul>
+
+    <p className="p--resume">Want to know more? Check out my <a href="https://www.linkedin.com/in/veronica-barcia3/">LinkedIn Profile</a>!</p>
+      </div>
       </section>
 
       <section className="background--lav">
